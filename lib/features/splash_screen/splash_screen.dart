a13e94f7233d318535fb02e65f17d5f206439554
mohamedmousa2003 <mousa1152003@gmail.com>
2025-04-30 11:12:27 +0200

    create project & design splash Screen & install package

---
+++ lib/features/splash_screen/splash_screen.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import '../../core/providers/animation_provider.dart';
+import '../../core/constant/images_assets.dart';
+import '../../features/onboarding/presentation/pages/onboarding_view.dart';
+import '../../core/utils/app_colors.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+  static const String routeName = "splash";
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  late AnimationManager animationManager;
+
+  @override
+  void initState() {
+    super.initState();
+    animationManager = AnimationManager();
+    animationManager.initialize(this,Duration(seconds: 3));
+
+    Future.delayed(Duration(seconds: 4), () {
+      Navigator.pushReplacementNamed(context, OnboardingView.routeName); // انتقل للصفحة التالية
+    });
+  }
+
+  @override
+  void dispose() {
+    animationManager.dispose();  // تعطيل الأنيميشنات
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColor.backgroundColor,
+      body: Center(
+        child: animationManager.isInitialized
+            ? Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            AnimatedBuilder(
+              animation: animationManager.sliderAnimationTop,
+              builder: (context, child) {
+                return SlideTransition(
+                  position: animationManager.sliderAnimationTop,
+                  child: Image.asset(
+                    ImagePng.logo,
+                    fit: BoxFit.fill,
+                    height: 100,
+                    width: 100,
+                  ),
+                );
+              },
+            ),
+            SizedBox(height: 24),
+            // عرض الأنيميشن السفلي
+            AnimatedBuilder(
+              animation: animationManager.sliderAnimationBottom,
+              builder: (context, child) {
+                return SlideTransition(
+                  position: animationManager.sliderAnimationBottom,
+                  child: Text(
+                    "A M U N",
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: Color(0xff2E2D2A),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                );
+              },
+            ),
+          ],
+        )
+            : CircularProgressIndicator(), // عرض مؤشر التحميل إذا لم تكن الأنيميشنات جاهزة
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'core/helper/observer.dart';
+import 'core/routes/app_routes.dart';
+import 'core/utils/app_colors.dart';
+import 'features/splash_screen/splash_screen.dart';
+
+void main() {
+  runApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        navigatorObservers: [
+          AppNavigatorObserver.instance.routeObserver,
+        ],
+        color: AppColor.backgroundColor,
+        // theme: AppLightTheme.theme,
+        // themeMode: ThemeMode.light,
+        initialRoute: SplashScreen.routeName,
+        onGenerateRoute: AppRoutes.onGenerateRoute,
+        home: SplashScreen(),
+      ),
+    );
+  }
+}
